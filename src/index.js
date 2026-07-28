2a4c1d4fa2e98385d3955864816c242566360750
Prince Sahu <sahuprince0387@gmail.com>
2026-07-28 21:27:50 +0530

    Fix main entry point in package.json and update dotenv path in index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import  app  from "./app.js";
 /* import connectDB from "./db"; */
 
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
 connectDB()
@@ -28,22 +28,3 @@ connectDB()
 
 
 
-/*import express from "express";
-const app = express()
- (async () =>{
-    try {
-       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-       app.on("error",(error)=>{
-        console.log("ERR: ",error);
-        throw error
-       })
-
-       app.listen(process.env.PORT, () =>{
-        console.log(`App is listing on port ${process.env.PORT}`)
-       })
-
-    } catch (error) {
-        console.error("ERROR",error)
-        throw err
-    }
-})() */
