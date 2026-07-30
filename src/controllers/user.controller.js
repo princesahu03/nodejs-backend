745aaed1bc168aa5cd186d11bf1cd5dbca9b3018
Prince Sahu <sahuprince0387@gmail.com>
2026-07-30 11:06:27 +0530

    Add sameSite attribute to cookie options for login, logout, and token refresh

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -151,7 +151,8 @@ const loginUser = asyncHandler( async (req, res) =>{
 
     const options = {
         httpOnly: true,
-        secure: true
+        secure: true,
+        sameSite: "none"
     }
 
     return res
@@ -184,7 +185,8 @@ const logoutUser = asyncHandler(async (req, res) => {
    ) 
    const options = {
     httpOnly: true,
-    secure: true
+    secure: true,
+    sameSite: "none"
 }
     return res
     .status(200)
@@ -219,7 +221,8 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     
         const options ={
             httpOnly: true,
-            secure: true
+            secure: true,
+            sameSite: "none"
         }
     
         const {accessToken, newRefreshToken} = await generateAccessAndRefreshToken(user._id)
